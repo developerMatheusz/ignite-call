caba21de85eecb280c214453ae3db1ab5f1732be
Matheus Zanela <matheusdeveloper001@gmail.com>
2023-01-31 11:04:47 -0300

    last commit

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-//Escreve somente /api pois o back-end e front-end estão no mesmo projeto. Então não é necessário escrever http://localhost:3000/api
 export const api = axios.create({
     baseURL: "/api"
 });
